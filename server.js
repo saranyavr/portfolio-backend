cce1a33b716f6d44f99ee76c9ade4efee413c087
saranyavr <saranukalidoss@gmail.com>
2025-08-22 09:40:21 +0530

    resume updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use("/api/contact", contactRouter);
 app.use("/api/projects", routerProj);
 app.use("/api/experience", routerExp);
 app.get("/api/resume", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'Saranya Rajmohan - Full Stack Developer(MERN) Resume (2).pdf'));
+  res.sendFile(path.join(__dirname, 'saranyarajmohan-developer-resume.pdf'));
 });
 
 
